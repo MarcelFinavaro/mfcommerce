d4a1e1d157c761c19fa56bb7e4dde08657c11143
Marcelo Finavaro <marcelfinavaro@gmail.com>
2026-02-11 11:24:32 -0300

    first commit

---
+++ src/main/java/com/devsuperior/dscommercem/DscommercemApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommercem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommercemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommercemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dscommercem/DscommercemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommercem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommercemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
